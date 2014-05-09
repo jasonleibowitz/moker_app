36be5037b5ae1118ab8fa724f7cc7d31bc02cde5
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-05-09 17:42:29 -0400

    added devise for user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,35 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
+
+  private
+  def user_params
+    return params.require(:user).permit(:email)
+  end
+
 end
