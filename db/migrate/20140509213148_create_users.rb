c61633739ed5c4ec465a6af537c47187334a6453
ByteDown <igor.yuzovitskiy@gmail.com>
2014-05-09 20:31:08 -0400

    facebook_auth

--- db/migrate/20140509213148_create_users.rb
+++ db/migrate/20140509213148_create_users.rb
@@ -1,6 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.integer :user_id
+      t.string :token
+      t.string :secret
+      t.string :name
+      t.string :length
     end
   end
 end
---
+++ db/migrate/20140509235128_create_authorization.rb
@@ -0,0 +1,6 @@
+class CreateAuthorization < ActiveRecord::Migration
+  def change
+    create_table :authorizations do |t|
+    end
+  end
+end
