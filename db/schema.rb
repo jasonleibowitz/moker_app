607e8e8592c9adeea4684e5f1f63590a913acf64
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-05-13 15:24:53 -0400

    updated migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513144117) do
+ActiveRecord::Schema.define(version: 20140513165053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,14 @@ ActiveRecord::Schema.define(version: 20140513144117) do
     t.string   "tip"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "tips", force: true do |t|
+    t.integer  "user_id"
+    t.string   "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "coffee_shop_id"
   end
 
   create_table "users", force: true do |t|
