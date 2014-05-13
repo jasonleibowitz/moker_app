ec1c96a2064043a8667c3fbc42afb711d3110b8b
ByteDown <igor.yuzovitskiy@gmail.com>
2014-05-12 20:00:07 -0400

    created search and results page to view the 10 closest coffeeshops based on the zip code entered

--- app/controllers/coffeeshops_controller.rb
+++ app/controllers/coffeeshops_controller.rb
@@ -1,11 +1,19 @@
-class CoffeeShopsController < ApplicationController
+class CoffeeshopsController < ApplicationController
 
   def index
-    @coffee_shops = CoffeeShop.all
+    # @coffee_shops = CoffeeShop.all
   end
 
   def show
     @coffee_shop = CoffeeShop.find(params[:id])
+  end
+
+  def search
+  end
+
+  def results
+    zip_code = params[:query]
+    @sorted_coffee_shops = CoffeeShop.coffeeshop_user_distance(zip_code)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140512134230) do
+ActiveRecord::Schema.define(version: 20140512162702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 20140512134230) do
     t.boolean "outlet_rating"
     t.boolean "workspace_rating"
     t.boolean "coffee_rating"
-    t.string  "comment"
+    t.string  "tip"
   end
 
   create_table "users", force: true do |t|
