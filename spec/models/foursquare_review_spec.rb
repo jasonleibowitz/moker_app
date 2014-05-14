e2afc57f00d229922422de5d806c31babb4afb4e
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-05-14 14:37:55 -0400

    added more foursquare review tests

--- spec/models/foursquare_review_spec.rb
+++ spec/models/foursquare_review_spec.rb
@@ -5,7 +5,9 @@ describe FoursquareReview do
   before :each do
     CoffeeShop.cache_from_foursquare(10010)
     @shop = CoffeeShop.first
-    @ranked_shops = CoffeeShop.where('total_wifi_reviews > ?', 0)
+    @wifi_ranked_shops = CoffeeShop.where('total_wifi_reviews > ?', 0)
+    @outlet_ranked_shops = CoffeeShop.where('total_outlet_reviews > ?', 0)
+    @workspace_ranked_shops = CoffeeShop.where('total_workspace_reviews > ?', 0)
     FoursquareReview.add_foursquare_reviews
   end
 
@@ -18,7 +20,9 @@ describe FoursquareReview do
   describe ".update_rating_based_on_foursquare_reviews" do
     it "should search all foursquare tips and edit each coffee shop's wifi, outlet and workspace ratings if it finds any positive or negative comments relating to any of these ratings" do
       FoursquareReview.update_rating_based_on_foursquare_reviews
-      expect(@ranked_shops.first.total_wifi_reviews).not_to eq(0)
+      expect(@wifi_ranked_shops.first.total_wifi_reviews).not_to eq(0)
+      expect(@outlet_ranked_shops.first.total_outlet_reviews).not_to eq(0)
+      expect(@workspace_ranked_shops.first.total_workspace_reviews).not_to eq(0)
     end
   end
 
