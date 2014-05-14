3ea8ecb6b7748a0d9d741bff8e00a490cc09f75a
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-05-14 14:37:44 -0400

    set up user test file

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,1 +1,7 @@
 require_relative '../spec_helper.rb'
+
+describe User do
+
+# Will have to use integration tests for OAuth. More info at https://github.com/intridea/omniauth/wiki/Integration-Testing
+
+end
