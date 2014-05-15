ad4ffed1f508a0a0edd4eb9dd056a4a730a42b35
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-05-15 14:47:49 -0400

    added about page with attribution for review icons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
+  get '/about' => 'welcome#about'
   resources :coffee_shops, shallow: true do
     resource :foursquarereviews
     resource :reviews
